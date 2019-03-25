71b5eb56fa57559c78ba5164019c2066244fec67
jaelyn coles <jaelyncoles@Matthews-MBP-2.localdomain>
2019-03-25 14:26:49 -0400

    started work on the notificaiton feature component

---
+++ farmtotable/src/components/notification.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="notification">
+    <b-alert
+      dismissible
+      v-model="showAlert"
+    >We have found providers in your area! Click HERE for directions.</b-alert>
+
+    <b-button @click="showAlert=true" variant="info" class="m-1">Submit</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showAlert: false
+    };
+  },
+  name: "notification",
+  methods: {}
+};
+</script>
