e7d335f403ac602795ec69fafa6fdccfbd2ad4d1
jaelyn coles <jaelyncoles@Matthews-MBP-2.localdomain>
2019-03-25 14:58:26 -0400

    button color green

--- farmtotable/src/components/notification.vue
+++ farmtotable/src/components/notification.vue
@@ -2,10 +2,11 @@
   <div id="notification">
     <b-alert
       dismissible
+      variant="success"
       v-model="showAlert"
     >We have found providers in your area! Click HERE for directions.</b-alert>
 
-    <b-button @click="showAlert=true" variant="info" class="m-1">Search</b-button>
+    <b-button @click="showAlert=true" variant="success" class="m-1">Search</b-button>
   </div>
 </template>
 
