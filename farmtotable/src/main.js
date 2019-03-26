11d71bd7b40d5c88e3c5dd7efc841dfb1db0d13f
Adam Fister <arfister@gmail.com>
2019-03-26 11:45:44 -0400

    installed map, vuex, and router - working branch clean - no errors, no bugs

--- farmtotable/src/main.js
+++ farmtotable/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+import 'es6-promise/auto'
+
 
 Vue.use(BootstrapVue)
+Vue.use(Vuex)
+Vue.use(VueRouter)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
