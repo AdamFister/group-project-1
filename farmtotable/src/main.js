a1a0fb7db1f595a66f3b0c6a12b7ba295ced6cb0
SamMoher <smohereight@gmail.com>
2019-03-26 14:26:33 -0400

    resolved more conflicts

--- farmtotable/src/main.js
+++ farmtotable/src/main.js
@@ -13,8 +13,6 @@ Vue.use(Vuex)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import router from './router'
-import store from './store'
 
 Vue.config.productionTip = false
 
