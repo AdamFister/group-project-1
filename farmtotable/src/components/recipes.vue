ff2d4f63ef0ef3b481b6d2b66f41402c2d6b4ad1
jaelyn coles <jaelyncoles@Matthews-MBP-2.localdomain>
2019-03-26 16:09:53 -0400

    started recipe template

---
+++ farmtotable/src/components/recipes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="recipes">
+    
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "recipes",
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
