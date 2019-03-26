be5e1966c8a0b50f8db844b4aff4fb3643dedb27
Adam Fister <arfister@gmail.com>
2019-03-26 14:01:09 -0400

    added counter

--- farmtotable/src/main.js
+++ farmtotable/src/main.js
@@ -4,11 +4,12 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'es6-promise/auto'
-
+import router from './router'
+import store from './store'
 
 Vue.use(BootstrapVue)
-Vue.use(Vuex)
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -16,5 +17,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
