1c42658722430cd82d2e68259b87d3c6cc8a07ad
jaelyn coles <jaelyncoles@Matthews-MBP-2.localdomain>
2019-03-26 11:47:41 -0400

    finished notification

---
+++ farmtotable/src/components/notification.vue
@@ -0,0 +1,23 @@
+<template>
+<div id="notification">
+<b-alert
+dismissible
+variant="success"
+v-model="showAlert"
+>We have found providers in your area! Click HERE for directions.</b-alert>
+
+<b-button @click="showAlert=true" variant="success" class="m-1">Search</b-button>
+</div>
+</template>
+
+<script>
+export default {
+name: "notification",
+data() {
+return {
+showAlert: false
+};
+},
+methods: {}
+};
+</script>
