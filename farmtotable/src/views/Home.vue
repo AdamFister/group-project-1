4ff778a06a42f5fba2ce5b09bcf17f204b2dbd06
jaelyn coles <jaelyncoles@Matthews-MBP-2.localdomain>
2019-03-26 15:01:22 -0400

    tested notification, it works great

--- farmtotable/src/views/Home.vue
+++ farmtotable/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <notification/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import notification from "@/components/notification.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    notification
   }
-}
+};
 </script>
