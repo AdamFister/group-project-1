6b19a9990fccb2b07118aa45df07859e3c04b554
SamMoher <smohereight@gmail.com>
2019-03-27 10:45:31 -0400

    going a different direction with mapping/location

--- farmtotable/src/App.vue
+++ farmtotable/src/App.vue
@@ -16,45 +16,46 @@ export default {
   name: "app",
   components: {
     MapComponent
-  },
-  mounted: function() {
-    var map, infoWindow;
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: 38.0355597, lng: -84.4924768 },
-      zoom: 8
-    });
-    infoWindow = new google.maps.InfoWindow;
+  }, 
+};
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-}
+//   mounted: function() {
+//     var map, infoWindow;
+//     map = new google.maps.Map(document.getElementById("map"), {
+//       center: { lat: 38.0355597, lng: -84.4924768 },
+//       zoom: 8
+//     });
+//     infoWindow = new google.maps.InfoWindow;
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      
-  };
+//         // Try HTML5 geolocation.
+//         if (navigator.geolocation) {
+//           navigator.geolocation.getCurrentPosition(function(position) {
+//             var pos = {
+//               lat: position.coords.latitude,
+//               lng: position.coords.longitude
+//             };
+
+//             infoWindow.setPosition(pos);
+//             infoWindow.setContent('Location found.');
+//             infoWindow.open(map);
+//             map.setCenter(pos);
+//           }, function() {
+//             handleLocationError(true, infoWindow, map.getCenter());
+//           });
+//         } else {
+//           // Browser doesn't support Geolocation
+//           handleLocationError(false, infoWindow, map.getCenter());
+//         }
+//       }
+// }
+
+//       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//         infoWindow.setPosition(pos);
+//         infoWindow.setContent(browserHasGeolocation ?
+//                               'Error: The Geolocation service failed.' :
+//                               'Error: Your browser doesn\'t support geolocation.');
+//         infoWindow.open(map);
 </script>
 
 
--- farmtotable/src/components/MapComponent.vue
+++ farmtotable/src/components/MapComponent.vue
@@ -1,19 +1,32 @@
 <template>
-  <div id="map"></div>
+  <div><a :href="getFarmerURL" target="_blank">My Location</a></div>
 </template>
 
 <script>
 export default {
   name: "MapComponent",
+  data (){
+    return {
+      location_url: "https://www.google.com/maps/dir/?api=1",
+      zoom_setting: "16z",
+      farmer_location_str: "@38.0355597,-84.4924768",
+    }
+  },
+  computed: {
+    getFarmerURL() {
+      return this.location_url;
+    }
+  }
 };
 </script>
 
 <style>
 #map {
-  height: 50vh;
+  font-size: 30px;
+  /* height: 50vh;
   width: 60vw;
   margin-left: auto;
   margin-right: auto;
-  border: 4px solid rgb(173, 30, 30);
+  border: 4px solid rgb(173, 30, 30); */
 }
 </style>
