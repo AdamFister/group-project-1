dfe6f5eb52851a6c69e34f2c0cd9e69e7c592517
Adam Fister <arfister@gmail.com>
2019-03-27 13:50:48 -0400

    everything working

--- farmtotable/src/views/Home.vue
+++ farmtotable/src/views/Home.vue
@@ -12,8 +12,6 @@
      <input @keyup.enter="addProduce" type="text" name="produce" placeholder="enter produce here" v-model="produce_text">
 
      <div>{{ allFarmers }}</div>
-      <div>{{ name }}</div>
-     <div>{{ location }}</div>
      <div v-for="item in produce">{{ item }}</div>
      <!-- <button @click="createProfile">Create Profile</button> -->
      <button @click="addFarmer">Add Profile</button>
